94a3f69268daf9476ed13538628a4c4284ed71a8
JaoGoncalves <jj5402315@gmail.com>
2026-04-14 19:43:31 -0300

    First commit: Inicialização do projeto

---
+++ src/test/java/com/example/sistematransporte/SistemaTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sistematransporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaTransporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
